207fd91c5766d93aa36e256c89652a37aeb2642a
sirsamhain <joelandapophis@gmail.com>
2020-03-06 07:22:40 -0600

    Changed profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ConwaysGameOfLifeComponent } from './conways-game-of-life/conways-game-of-life.component';
+import { ContactComponent } from './contact/contact.component';
+import { QuickSortComponent } from './sorting/quick-sort/quick-sort.component';
+import { BubbleSortComponent } from './sorting/bubble-sort/bubble-sort.component';
+import { InsertionSortComponent } from './sorting/insertion-sort/insertion-sort.component';
+import { SelectionSortComponent } from './sorting/selection-sort/selection-sort.component';
+import { DijkstraComponent } from './pathFinding/dijkstra/dijkstra.component';
+import { DoublePendulumComponent } from './physics/double-pendulum/double-pendulum.component';
+import { FourierComponent } from './math/fourier/fourier.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:HomeComponent},
+  {path:"conways-game-of-life", component:ConwaysGameOfLifeComponent},
+  {path:"contact", component:ContactComponent},
+  {path:"sorting/quick-sort", component: QuickSortComponent},
+  {path:"sorting/bubble-sort", component: BubbleSortComponent},
+  {path:"sorting/insertion-sort", component: InsertionSortComponent},
+  {path:"sorting/selection-sort", component: SelectionSortComponent},
+  {path:"path-finding/dijkstra", component: DijkstraComponent},
+  {path:"physics/double-pendulum", component: DoublePendulumComponent},
+  {path:"maths/fourier", component: FourierComponent},
+  {path:"**", component:NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConwaysGameOfLifeComponent } from './conways-game-of-life/conways-game-of-life.component';
-
 import { MatSliderModule } from "@angular/material/slider"
 import { MatMenuModule } from "@angular/material/menu"
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NavbarComponent } from './navbar/navbar.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './home/home.component';
-import { RouterModule } from '@angular/router';
-
 import { MatIconModule } from '@angular/material/icon';
 import { ContactComponent } from './contact/contact.component';
 import { QuickSortComponent } from './sorting/quick-sort/quick-sort.component';
@@ -23,7 +19,6 @@ import { TimingService } from './services/timing-service.service';
 import { AngularFireFunctionsModule } from '@angular/fire/functions'
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
-import { environment } from 'src/environments/environment';
 import { EmailService } from './services/email.service';
 import { HttpClientModule } from '@angular/common/http'
 import { fb } from 'config';
@@ -33,20 +28,6 @@ import { DoublePendulumComponent } from './physics/double-pendulum/double-pendul
 import { FourierComponent } from './math/fourier/fourier.component';
 import { InsertionSortComponent } from './sorting/insertion-sort/insertion-sort.component';
 
-
-const routes = [
-  {path:"", component:HomeComponent},
-  {path:"conways-game-of-life", component:ConwaysGameOfLifeComponent},
-  {path:"contact", component:ContactComponent},
-  {path:"sorting/quick-sort", component: QuickSortComponent},
-  {path:"sorting/bubble-sort", component: BubbleSortComponent},
-  {path:"sorting/insertion-sort", component: InsertionSortComponent},
-  {path:"sorting/selection-sort", component: SelectionSortComponent},
-  {path:"path-finding/dijkstra", component: DijkstraComponent},
-  {path:"physics/double-pendulum", component: DoublePendulumComponent},
-  {path:"maths/fourier", component: FourierComponent},
-  {path:"**", component:NotFoundComponent},
-]
 
 @NgModule({
   declarations: [
@@ -80,8 +61,7 @@ const routes = [
     AngularFireFunctionsModule,
     AngularFireModule,
     AngularFireDatabaseModule,
-    HttpClientModule,
-    RouterModule.forRoot(routes)
+    HttpClientModule
   ],
   providers: [
     TimingService,
